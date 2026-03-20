dabrain34/Vulkan-Video-Samples

commit 25db66e979a1dd70eb73060e2890d11bf2f487ab

encoder: Deal with the Vulkan chained stuctures

vk_video_encoder/libs/VkVideoEncoder/VkVideoEncoder.cpp
@@ -1696,12 +1696,9 @@ VkResult VkVideoEncoder::HandleCtrlCmd(VkSharedBaseObj<VkVideoEncodeFrameInfo>& 
         encodeFrameInfo->qualityLevelInfo.sType  = VK_STRUCTURE_TYPE_VIDEO_ENCODE_QUALITY_LEVEL_INFO_KHR;
         encodeFrameInfo->qualityLevelInfo.qualityLevel = encodeFrameInfo->qualityLevel;
         if (pNext != nullptr) {
-            if (encodeFrameInfo->rateControlInfo.pNext == nullptr) {
-                encodeFrameInfo->rateControlInfo.pNext = pNext;
-            } else {
-                ((VkBaseInStructure*)(encodeFrameInfo->rateControlInfo.pNext))->pNext = pNext;
-            }
+            vk::ChainNextVkStruct(encodeFrameInfo->rateControlInfo, *pNext);
         }
+
         pNext = (VkBaseInStructure*)&encodeFrameInfo->qualityLevelInfo;
     }
 
@@ -1724,12 +1721,9 @@ VkResult VkVideoEncoder::HandleCtrlCmd(VkSharedBaseObj<VkVideoEncodeFrameInfo>& 
         m_beginRateControlInfo = encodeFrameInfo->rateControlInfo;
 
         if (pNext != nullptr) {
-            if (encodeFrameInfo->rateControlInfo.pNext == nullptr) {
-                encodeFrameInfo->rateControlInfo.pNext = pNext;
-            } else {
-                ((VkBaseInStructure*)(encodeFrameInfo->rateControlInfo.pNext))->pNext = pNext;
-            }
+            vk::ChainNextVkStruct(encodeFrameInfo->rateControlInfo, *pNext);
         }
+
         pNext = (VkBaseInStructure*)&encodeFrameInfo->rateControlInfo;
     }
 
@@ -1809,7 +1803,8 @@ VkResult VkVideoEncoder::RecordVideoCodingCmd(VkSharedBaseObj<VkVideoEncodeFrame
         vkDevCtx->CmdControlVideoCodingKHR(cmdBuf, &renderControlInfo);
 
         m_beginRateControlInfo = *(VkVideoEncodeRateControlInfoKHR*)encodeFrameInfo->pControlCmdChain;
-        ((VkBaseInStructure*)(m_beginRateControlInfo.pNext))->pNext = NULL;
+        // Do not walk the chain, otherwise we end up creating a loop here.
+        m_beginRateControlInfo.pNext = (VkBaseInStructure*)(&encodeFrameInfo->pControlCmdChain);
     }
 
     if (m_videoMaintenance1FeaturesSupported)
@@ -1821,10 +1816,12 @@ VkResult VkVideoEncoder::RecordVideoCodingCmd(VkSharedBaseObj<VkVideoEncodeFrame
         videoInlineQueryInfoKHR.firstQuery = querySlotId;
         videoInlineQueryInfoKHR.queryCount = numQuerySamples;
         VkBaseInStructure* pStruct = (VkBaseInStructure*)&encodeFrameInfo->encodeInfo;
-        while (pStruct->pNext) pStruct = (VkBaseInStructure*)pStruct->pNext;
-        pStruct->pNext = (VkBaseInStructure*)&videoInlineQueryInfoKHR;
+        vk::ChainNextVkStruct(*pStruct, videoInlineQueryInfoKHR);
 
         vkDevCtx->CmdEncodeVideoKHR(cmdBuf, &encodeFrameInfo->encodeInfo);
+
+        // Remove the stack pointer from the chain, causes a use after free otherwise in GetEncodeFrameInfoH264
+        encodeFrameInfo->encodeInfo.pNext = videoInlineQueryInfoKHR.pNext;
     }
     else
     {

common/libs/VkCodecUtils/Helpers.h
@@ -320,7 +320,7 @@ inline VkResult WaitAndGetStatus(const VkInterfaceFunctions* vkIf, VkDevice devi
  }
 
 template<typename NodeType, typename ChainedNodeType>
-inline VkBaseInStructure* ChainNextVkStruct(NodeType& node, ChainedNodeType& nextChainedNode) {
+inline void ChainNextVkStruct(NodeType& node, ChainedNodeType& nextChainedNode) {
     // make sure the node is of type VkBaseInStructure
     static_assert(offsetof(NodeType, sType) == offsetof(VkBaseInStructure, sType),
                   "NodeType does not have sType at the same offset as VkBaseInStructure");
@@ -341,16 +341,16 @@ inline VkBaseInStructure* ChainNextVkStruct(NodeType& node, ChainedNodeType& nex
                   "ChainedNodeType must be a standard-layout type");
 
     assert(node.sType > 0);
-    VkBaseInStructure* pNode = (VkBaseInStructure*)&node;
-    while (pNode->pNext != nullptr) {
-         pNode = (VkBaseInStructure*)pNode->pNext;
-     }
-     pNode->pNext = (VkBaseInStructure*)&nextChainedNode;
-     // make sure the nextChainedNode is of type VkBaseInStructure
-     assert(nextChainedNode.sType > 0);
-     assert(nextChainedNode.pNext == nullptr);
-     return (VkBaseInStructure*)nextChainedNode.pNext;
- }
+    VkBaseInStructure* pNode = (VkBaseInStructure*)(&node);
+    VkBaseInStructure* pNextNode = (VkBaseInStructure*)(&nextChainedNode);
+
+    // The incoming object may not have anything chained.
+    assert(pNextNode->pNext == nullptr);
+
+    // Inserts the incoming object at the beginning of the list.
+    pNextNode->pNext = pNode->pNext;
+    pNode->pNext = pNextNode;
+}
 
 class DeviceUuidUtils
 {

vk_video_encoder/libs/VkVideoEncoder/VkVideoEncoder.h
@@ -58,7 +58,7 @@ public:
     {
         VkStructureType GetType() {
             return (encodeInfo.pNext == nullptr) ?
-                    VK_STRUCTURE_TYPE_VIDEO_ENCODE_INFO_KHR : ((VkBaseInStructure*)encodeInfo.pNext)->sType;
+                    VK_STRUCTURE_TYPE_VIDEO_ENCODE_INFO_KHR : reinterpret_cast<const VkBaseInStructure*>(encodeInfo.pNext)->sType;
         }
 
         VkVideoEncodeFrameInfo(const void* pNext = nullptr)

commit 7bb6882787065ac2ba62e16d9b1528725466ed59

encoder: fix invalid pNext pointer in rate control chain
The pNext field was set to the address of the pControlCmdChain pointer
itself rather than a valid Vulkan structure, causing undefined behavior.
Null-terminate the chain instead to avoid loops.

Fixes a regression introduced in 25db66e ("encoder: Deal with the Vulkan
chained stuctures").

vk_video_encoder/libs/VkVideoEncoder/VkVideoEncoder.cpp
@@ -1909,7 +1909,7 @@ VkResult VkVideoEncoder::RecordVideoCodingCmd(VkSharedBaseObj<VkVideoEncodeFrame
 
         m_beginRateControlInfo = *(VkVideoEncodeRateControlInfoKHR*)encodeFrameInfo->pControlCmdChain;
         // Do not walk the chain, otherwise we end up creating a loop here.
-        m_beginRateControlInfo.pNext = (VkBaseInStructure*)(&encodeFrameInfo->pControlCmdChain);
+        m_beginRateControlInfo.pNext = NULL;
     }
 
     if (m_videoMaintenance1FeaturesSupported)