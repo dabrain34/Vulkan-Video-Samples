87080b097de438a7d832a5dd41e755b23cc9447b
Eduardo Lima <elima@igalia.com>
2026-07-20 15:21:20 +0200

    VkEncoderConfigAV1: remove unused picture feedback QP parameters
    The values parsed from "--pictureFeedback --params" were never read.
    Remove them and the related help text.

--- vk_video_encoder/libs/VkVideoEncoder/VkEncoderConfigAV1.cpp
+++ vk_video_encoder/libs/VkVideoEncoder/VkEncoderConfigAV1.cpp
@@ -141,14 +141,6 @@ int EncoderConfigAV1::DoParseArguments(int argc, const char* argv[])
             }
         } else if (args[i] == "--pictureFeedback") {
             enablePictureFeedback = true;
-
-            if (((i + 1) < argc) && (args[i + 1] == "--params")) {
-                ++i;
-                customPictureFeedbackParams = true;
-                READ_PARAM(i, pictureFeedbackAvgQp, int32_t);
-                READ_PARAM(i, pictureFeedbackMinQp, int32_t);
-                READ_PARAM(i, pictureFeedbackMaxQp, int32_t);
-            }
         } else if (args[i] == "--pixelFeedback") {
             enablePixelFeedback = true;
         } else if (args[i] == "--skippedPixelFeedback") {
